dc87f0d375e1ad63e8098f0b3f94a02904f31f07
Simone Amaral <simone.amaral@global.ntt>
2024-09-23 20:49:56 -0300

    task 4 revisão

---
+++ task4.py
@@ -0,0 +1,5 @@
+''' 4 - Crie um programa que receba uma variável do usuário, armazene a mesma em 
+uma variável e escreva no console a variável digitada pelo usuário. '''
+
+nome = input('Digite seu nome: ')
+print(nome)
