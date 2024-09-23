23f5fd0eb22f74f33dbaef92dc8a60df2f23c71a
Simone Amaral <simone.amaral@global.ntt>
2024-09-23 20:46:48 -0300

    task 2 string e texto

---
+++ task2.py
@@ -0,0 +1,23 @@
+''' 2 - Adicione ao programa anterior comandos para escrever na tela. '''
+
+sigla1 = 'ADP'
+sigla2 = 'AM'
+sigla3 = 'BCP'
+sigla4 = 'CM'
+sigla5 = 'CS'
+
+print('Siglas salvas nas variáveis: ')
+print(sigla1, sigla2, sigla3, sigla4, sigla5)
+
+''' - Por que em print("Olá # ntt") não é um comentário? '''
+# Porque esta dentro da função print() como uma string " ", para utilizar #
+# para comentários precisa ser no inicio da linha ou após o código.
+
+''' - Qual a diferença entre usar aspas simples (') e aspas duplas (") ? '''
+# Ambas armazenam a mesma string
+print('Olá, "Mundo"!') # quando precisar imprimir aspas duplas use aspas simples no print
+print("Olá, 'Mundo'!") # quando precisar imprimir aspas simples use aspas duplas no print
+
+# Outro opção:
+print('Olá, \'Mundo\'') # utilizar a barra e a apas \'
+print("Olá, \"Mundo\"") # utilizar a barra e a apas \"
