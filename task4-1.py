186d90a3987e79a6348bd901c596c6b6b5d3f049
Simone Amaral <simone.amaral@global.ntt>
2024-09-23 20:59:32 -0300

    task 4.1 formatando strings

---
+++ task4-1.py
@@ -0,0 +1,6 @@
+''' 4.1 - Crie um programa que receba do usuário um texto e escreva na tela um 
+texto junto com o texto digitado pelo usuário. Desafio: use apenas duas linhas 
+para resolver esse código. '''
+
+frase = input('Digite uma frase: ')
+print(f'Frase digitada: "{frase}"')
