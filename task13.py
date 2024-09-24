d53c0d915afd10d6288d825a8179c3adc3b59eb1
Simone Amaral <simone.amaral@global.ntt>
2024-09-24 16:14:41 -0300

    dicionários

---
+++ task13.py
@@ -0,0 +1,19 @@
+''' A aplicação de dicionários vai ser fundamental para o projeto que queremos
+trabalhar no futuro. Os dicionários (ou dict), são um modo de armazenar dados 
+assim como uma lista, mas, ao invés de usar apenas números para obter dados, é 
+possível usar quase tudo. Isso permite tratar um dict como um banco de dados 
+para armazenar e organizar dados. '''
+
+''' Um dict é usado para mapear ou associar as coisas que você deseja armazenar nas chaves necessárias para obtê-las. '''
+
+''' - Qual é a diferença entre um dicionário e uma lista? '''
+# A dirença está em como armazenamos e acessamos os dados.
+# Dicionário é uma coleção de pares chave-valor, sendo possível acessar os itens de várias maneiras 
+# pela chave, valor ou ambos.
+# Lista é uma coleção ordenada de itens, podendo acessar os itens pelo seu índice. 
+
+''' - Por que eu usaria um dicionário? '''
+# Porque permite acessar facilmente valores usando suas chaves
+# Ideal para armazenar dados que tem uma relação, dados complexos...
+
+
